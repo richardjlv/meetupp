26d917f5ba31bdcf7b579be818be1437d3627bd6
richardjlv <richardjunio39@gmail.com>
2020-03-06 16:42:58 -0300

    Adding react-toastify

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import '~/config/reactotronConfig';
 import Routes from '~/Routes';
 import history from '~/services/history';
+import { store } from '~/store';
 import GlobalStyle from '~/styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer />
+      </Router>
+    </Provider>
   );
 }
 
