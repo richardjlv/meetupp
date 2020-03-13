6cf74edcadf3c256f03d8408e1b2d6dc78d27194
richardjlv <richardjunio39@gmail.com>
2020-03-12 23:15:53 -0300

    Adding Meetup Edit Page

--- web/src/pages/Meetup/index.js
+++ web/src/pages/Meetup/index.js
@@ -1,21 +1,25 @@
+import { Form, Input } from '@rocketseat/unform';
 import React, { useEffect, useState } from 'react';
 import { MdEdit, MdDeleteForever } from 'react-icons/md';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
+import BannerInput from './BannerInput';
 import { Container, MeetupHeader, Date } from './styles';
-import image from '~/assets/Bitmap.png';
 import api from '~/services/api';
+import history from '~/services/history';
 import { formatDate } from '~/util/format';
 
 export default function Meetup() {
   const { id } = useParams();
   const [meetup, setMeetup] = useState({});
+  const [editing, setEditing] = useState(false);
 
   useEffect(() => {
     async function loadMeetup() {
       const response = await api.get('/users');
 
-      const data = response.data.find(m => m.id === Number(id[1]));
+      const data = response.data.find(m => m.id === Number(id));
 
       setMeetup({
         ...data,
@@ -24,31 +28,82 @@ export default function Meetup() {
     }
 
     loadMeetup();
-  }, [id]);
+  }, [id, editing]);
 
-  function handleDelete() { }
+  async function handleDelete() {
+    try {
+      await api.delete(`/meetups/${id}`);
 
-  return (
-    <Container>
-      <MeetupHeader>
-        <strong>{meetup.name}</strong>
-        <div>
-          <button type="button">
-            <MdEdit color="#fff" size={24} />
-            <p>Editar</p>
-          </button>
-          <button type="button" onClick={handleDelete}>
-            <MdDeleteForever color="#fff" size={24} />
-            <p>Delete</p>
-          </button>
-        </div>
-      </MeetupHeader>
-      <img src={image} alt="evento" />
-      <p>{meetup.description}</p>
-      <Date>
-        <span>{meetup.dateFormatted}</span>
-        <span>Rua Guilherme Gembala, 260</span>
-      </Date>
-    </Container>
-  );
+      toast.success('Meetup deletado com sucesso');
+
+      history.push('/dashboard');
+    } catch (err) {
+      toast.error('Não foi possível deletar o Meetup');
+    }
+  }
+
+  async function handleEdit() {
+    setEditing(!editing);
+  }
+
+  async function handleSubmit(data) {
+    try {
+      await api.put(`/meetups/${id}`, data);
+
+      toast.success('Meetup atualizado com sucesso');
+
+      handleEdit();
+    } catch (err) {
+      toast.error('Não foi possível atualizar o Meetup, verifique seus dados!');
+    }
+  }
+
+  function MeetupContent() {
+    return (
+      <>
+        <MeetupHeader>
+          <strong>{meetup.name}</strong>
+          <div>
+            <button type="button" onClick={handleEdit}>
+              <MdEdit color="#fff" size={24} />
+              <p>Editar</p>
+            </button>
+            <button type="button" onClick={handleDelete}>
+              <MdDeleteForever color="#fff" size={24} />
+              <p>Delete</p>
+            </button>
+          </div>
+        </MeetupHeader>
+        <img src={meetup.banner ? meetup.banner.url : ''} alt="evento" />
+        <p>{meetup.description}</p>
+        <Date>
+          <span>{meetup.dateFormatted}</span>
+          <span>{meetup.location}</span>
+        </Date>
+      </>
+    );
+  }
+
+  function EditMeetup() {
+    return (
+      <>
+        <Form initialData={meetup} onSubmit={handleSubmit}>
+          <Input name="name" placeholder="Nome do Meetup" />
+          <BannerInput name="banner_id" />
+          <Input
+            name="description"
+            type="text"
+            multiline
+            placeholder="Descrição do Meetup"
+          />
+          <Input name="dateFormatted" placeholder="Data do Meetup" />
+          <Input name="location" placeholder="Localização do Meetup" />
+
+          <button type="submit">Atualizar Meetup</button>
+        </Form>
+      </>
+    );
+  }
+
+  return <Container>{editing ? <EditMeetup /> : <MeetupContent />}</Container>;
 }
--- web/src/pages/Meetup/styles.js
+++ web/src/pages/Meetup/styles.js
@@ -1,3 +1,4 @@
+import { darken } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
@@ -7,6 +8,7 @@ export const Container = styled.div`
 
   img {
     max-width: 900px;
+    max-height: 300px;
     width: calc(100% - 20px);
     padding: 0 10px;
     margin: 0 10px;
@@ -15,9 +17,62 @@ export const Container = styled.div`
   > p {
     color: #fff;
     font-size: 16px;
-    line-height: 32px;
+    line-height: 20px;
     margin: 10px;
     padding: 10px;
+    white-space: pre-wrap;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 25px 10px;
+    padding: 25px 10px;
+
+    input {
+      background: rgba(0, 0, 0, 0.2);
+      width: 100%;
+      max-height: 600px;
+      border: 0;
+      border-radius: 4px;
+      height: 50px;
+      margin-bottom: 10px;
+      margin-top: 10px;
+      padding: 10px 15px;
+      color: rgba(255, 255, 255, 0.7);
+      resize: auto;
+    }
+
+    textarea {
+      line-height: 20px;
+      margin: 15px 0;
+      height: 200px;
+      align-self: stretch;
+      background: rgba(0, 0, 0, 0.1);
+      color: #fff;
+      padding: 15px;
+      border: 0;
+      border-radius: 4px;
+    }
+
+    button {
+      background: #4dbaf9;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-weight: bold;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 10px 25px;
+      margin-top: 20px;
+      font-size: 16px;
+
+      &:hover {
+        background: ${darken(0.05, '#4dbaf9')};
+      }
+    }
   }
 `;
 
@@ -55,6 +110,7 @@ export const MeetupHeader = styled.div`
       p {
         display: block;
         color: #fff;
+        font-weight: bold;
         margin-left: 10px;
       }
     }
