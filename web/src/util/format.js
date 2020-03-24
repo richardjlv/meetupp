9197e4bcc1ea9e8d555c78b1ce72bad2df956ef1
richardjlv <richardjunio39@gmail.com>
2020-03-23 23:28:30 -0300

    Correcting the date formatting

--- web/src/util/format.js
+++ web/src/util/format.js
@@ -2,6 +2,6 @@ import { format, parseISO } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
 
 export const formatDate = date =>
-  format(parseISO(date), "dd 'de' MMMM', às' HH'h' ", {
+  format(parseISO(date), "dd 'de' MMMM', às' HH'h'", {
     locale: pt,
   });
