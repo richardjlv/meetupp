c9072761d9d283e9fca217c5cbe9e283c03531a7
richardjlv <richardjunio39@gmail.com>
2020-03-28 17:28:28 -0300

    Adding signOut

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { Container, Content, Profile } from './styles';
 import logo from '~/assets/logo.svg';
+import { signOut } from '~/store/modules/auth/actions';
 
 export default function Header() {
   const { name } = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
 
   return (
     <Container>
@@ -22,7 +28,9 @@ export default function Header() {
             <strong>{name}</strong>
             <Link to="/profile">Meu perfil</Link>
           </Profile>
-          <button type="button">Sair</button>
+          <button type="button" onClick={handleSignOut}>
+            Sair
+          </button>
         </aside>
       </Content>
     </Container>
