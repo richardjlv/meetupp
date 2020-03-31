d3c013c77986028fbbbc56ac48e780a13debe689
richardjlv <richardjunio39@gmail.com>
2020-03-30 21:20:47 -0300

    Configuring redux-persist

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -3,18 +3,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import '~/config/reactotronConfig';
 import Routes from './routes';
-import store from './store';
+import { store, persistor } from '~/store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar barStyle="light-content" backgroundColor="#191920" />
-        <Routes />
-      </NavigationContainer>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <StatusBar barStyle="light-content" backgroundColor="#191920" />
+          <Routes />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
