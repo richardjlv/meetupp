bc8a7e7020379b50f41f1b45efa8020355fbea75
richardjlv <richardjunio39@gmail.com>
2020-03-30 21:18:10 -0300

    Changing routes

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,19 +1,35 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 
+import Dashboard from '~/pages/Dashboard';
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function Routes() {
   const signed = false;
 
+  function App() {
+    return (
+      <Tab.Navigator initialRouteName="Dashboard">
+        <Tab.Screen name="Dashboard" component={Dashboard} />
+      </Tab.Navigator>
+    );
+  }
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="SignIn" component={SignIn} />
-      <Tab.Screen name="SignUp" component={SignUp} />
-    </Tab.Navigator>
+    <Stack.Navigator initialRouteName="SignIn" headerMode="none">
+      {signed ? (
+        <Stack.Screen name="App" component={App} />
+      ) : (
+          <>
+            <Stack.Screen name="SignIn" component={SignIn} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+          </>
+        )}
+    </Stack.Navigator>
   );
 }
