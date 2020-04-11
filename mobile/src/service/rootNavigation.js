bae752d5061f7def13a5181cb55f88f8316d2da8
richardjlv <richardjunio39@gmail.com>
2020-04-11 15:52:43 -0300

    Adding api and rootNavigation

--- mobile/src/service/rootNavigation.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const navigationRef = React.createRef();
-
-export function navigate(name, params = {}) {
-  return navigationRef.current?.navigate(name, params);
-}
